3a6e92057f35f09df484105c289666f2c2a81a16
Luke Mills <luke@aligent.com.au>
2013-04-22 15:38:55 +0930

    Added iptables.

--- roles/magento.rb
+++ roles/magento.rb
@@ -2,7 +2,8 @@ name "magento"
 description 'Installs all the requisite packages to run the Magento application'
 run_list(
     'recipe[magento]',          # Installs php etc...
-    'recipe[magento::nginx]'    # Installs nginx (obviously)
+    'recipe[magento::nginx]',   # Installs nginx (obviously)
+    'recipe[simple_iptables]'
 )
 
 default_attributes(
