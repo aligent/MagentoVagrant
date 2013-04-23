f29051c9efad0ce4cc526ca6c364d1a4a53d6f25
Luke Mills <luke@aligent.com.au>
2013-04-23 10:17:19 +0930

    Removed varnish cookbook, and added chef-varnish cookbook, as it has better support for Centos, and uses the latest version of Varnish.

--- roles/varnish.rb
+++ roles/varnish.rb
@@ -1,8 +1,12 @@
 name "varnish"
 description 'Installs the Varnish web server'
 run_list(
-    'recipe[varnish]'
+    'recipe[chef-varnish]'
 )
 
 default_attributes(
+    :varnish => {
+        :VARNISH_LISTEN_PORT => 6081,
+        :VARNISH_BACKEND_PORT => 80
+    }
 )
