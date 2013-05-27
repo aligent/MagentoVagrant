751b7b0b40219db08648f50db6887239c7c665b8
root <root@www03.aligent.com.au>
2013-05-27 12:39:23 +0930

    Let Chef find it's own path rather than hardcoding it.

--- solo.rb
+++ solo.rb
@@ -1,5 +1,4 @@
-#root = File.absolute_path(File.dirname(__FILE__))
-root = '/home/luke/chef/'
+root = File.absolute_path(File.dirname(__FILE__))
 file_cache_path root
 cookbook_path [root + '/cookbooks', root + '/cookbooks-aligent', root + '/cookbooks-local']
 role_path root + '/roles'
